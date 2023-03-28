3eb1f78356071c2a22828ed3a07892a43e4a7ffc
kjh-busan <picsmash7@gmail.com>
2023-03-29 02:05:14 +0900

    feat: add api error case

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/film/");
+      const response = await fetch("https://swapi.dev/api/films/");
 
       if (!response.ok) {
         console.log("Fetching is error");
@@ -37,21 +37,20 @@ function App() {
     setIsLoading(false);
   }
 
+  let content = <p>Now Loading...</p>;
+
+  if (error) content = <p>{error}</p>;
+  if (!isLoading && moviesList.length === 0 && !error)
+    content = <p>Found Not Movies.</p>;
+  if (!isLoading && moviesList.length > 0)
+    content = <MoviesList movies={moviesList} />;
+
   return (
     <React.Fragment>
       <section>
         <button onClick={fetchMovieHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && moviesList.length > 0 && (
-          <MoviesList movies={moviesList} />
-        )}
-        {!isLoading && moviesList.length === 0 && !error && (
-          <p>Found Not Movies.</p>
-        )}
-        {isLoading && <p>Now Loading...</p>}
-        {!isLoading && error && <p>{error}</p>}
-      </section>
+      <section>{content}</section>
     </React.Fragment>
   );
 }
