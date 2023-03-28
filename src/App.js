e0da11b7c4b42204d230c08fa0a66b75987dd4dd
kjh-busan <picsmash7@gmail.com>
2023-03-29 01:26:42 +0900

    feat: add async await promise

--- src/App.js
+++ src/App.js
@@ -6,23 +6,18 @@ import "./App.css";
 function App() {
   const [moviesList, setMovieList] = useState([]);
 
-  function fetchMovieHandler() {
-    fetch("https://swapi.dev/api/films/")
-      .then((response) => {
-        return response.json();
-      })
-      .then((data) => {
-        // console.log(data.results.length);
-        const transFormedMovie = data.results.map((movie) => {
-          return {
-            id: movie.episode_id,
-            title: movie.title,
-            openingText: movie.opening_crawl,
-            relreaseDate: movie.relrease_date,
-          };
-        });
-        setMovieList(transFormedMovie);
-      });
+  async function fetchMovieHandler() {
+    const response = await fetch("https://swapi.dev/api/films/");
+    const data = await response.json();
+    const transFormedMovie = data.results.map((movie) => {
+      return {
+        id: movie.episode_id,
+        title: movie.title,
+        openingText: movie.opening_crawl,
+        relreaseDate: movie.relrease_date,
+      };
+    });
+    setMovieList(transFormedMovie);
   }
 
   return (
